fe5cc022e24ac62028aa3fc5b7fca229b5e63a76
Jaan Tollander de Balsch <jaan.tollander.de.balsch@csc.fi>
2024-04-25 10:49:48 +0300

    initial commit

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+USAGE="
+Install Octave using Ansible.
+
+USAGE
+    install.sh <systemname> <target> <version>
+
+EXAMPLES
+    # Install Octave version 9.1.0 to Puhti:
+
+    install.sh puhti octave 9.1.0
+"
+
+case ${1:-} in
+    local|localhost)
+        GROUPNAME=group_localhost
+        ;;
+    puhti)
+        GROUPNAME=group_puhti
+        ;;
+    help|-h|--help)
+        echo "$USAGE"
+        exit 0
+        ;;
+    *)
+        echo "$USAGE" >&2
+        exit 1
+        ;;
+esac
+shift 1
+
+TARGET=$1
+shift 1
+
+VERSION=$1
+shift 1
+
+ansible-playbook -i hosts.yaml -l "$GROUPNAME" -e "version=$VERSION" "$TARGET/install.yaml" "$@"
---
+++ octave/version/9.1.0/octave.lua
@@ -0,0 +1,13 @@
+-- Version string
+version = myModuleVersion()
+
+-- Path to the application directory.
+appl_dir = "/appl/soft/math/octave"
+
+-- Add wrapper scripts to the path.
+bin_dir = pathJoin(appl_dir, version, "bin")
+prepend_path("PATH", bin_dir)
+
+-- Path to the matlab container image to run using apptainer or singularity.
+image = pathJoin(appl_dir, version, "octave.sif")
+setenv("OCTAVE_SING_IMAGE", image)
